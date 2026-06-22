25ae804944da464d593142e8524eaa919506fea8
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-06-22 11:45:52 -0400

    Fix error when no candidate links found

--- R/evaluate_candidate_parent_pool.R
+++ R/evaluate_candidate_parent_pool.R
@@ -43,86 +43,97 @@ evaluate_candidate_parent_pool = function(baby_to_match, parents_to_match, key_b
       )
     }
 
-    ## Subset parents_to_match to candidate pool
-    candidates_parents_data = parents_to_match[parents_to_match[, id_parent] %in% candidate_pool, ]
-    ### Keep only columns for the ID and matching keys
-    candidates_parents_data = candidates_parents_data[, c(id_parent, unique(key_parent))]
+    if (length(candidate_pool) > 0) {
+      ## Subset parents_to_match to candidate pool
+      candidates_parents_data = parents_to_match[parents_to_match[, id_parent] %in% candidate_pool, ]
+      ### Keep only columns for the ID and matching keys
+      candidates_parents_data = candidates_parents_data[, c(id_parent, unique(key_parent))]
 
-    ## Calculate the similarity score per variable and per candidate parent
-    return_candidates_parents_data = data.frame()
-    for (c in 1:nrow(candidates_parents_data)) {
-      ### Loop through keys
-      candidates_parents_data_c = candidates_parents_data[c, ]
-      for (k in 1:length(key_baby)) {
-        #### Make sure key is not missing in baby's data
-        if (!all(is.na(baby_to_match[key_baby[k]]))) {
-          ##### Do combined (deterministic --> probabilistic linkage)
-          ###### Based on the kth baby/parent keys provided
-          link_on_k = combined_baby_parent_linkage(
-            baby_to_match = baby_to_match,
-            parents_to_match = candidates_parents_data_c,
-            key_baby = key_baby[k],
-            key_parent = key_parent[k],
-            jaro_col_name = jaro_col_name[k],
-            skip_probabilistic = key_baby[k] %in% deterministic_only
-          )
-          if (nrow(link_on_k$match) > 0) {
-            if (link_on_k$type == "deterministic") {
-              link_on_k$match[, jaro_col_name[k]] = 1
+      ## Calculate the similarity score per variable and per candidate parent
+      return_candidates_parents_data = data.frame()
+      for (c in 1:nrow(candidates_parents_data)) {
+        ### Loop through keys
+        candidates_parents_data_c = candidates_parents_data[c, ]
+        for (k in 1:length(key_baby)) {
+          #### Make sure key is not missing in baby's data
+          if (!all(is.na(baby_to_match[key_baby[k]]))) {
+            ##### Do combined (deterministic --> probabilistic linkage)
+            ###### Based on the kth baby/parent keys provided
+            link_on_k = combined_baby_parent_linkage(
+              baby_to_match = baby_to_match,
+              parents_to_match = candidates_parents_data_c,
+              key_baby = key_baby[k],
+              key_parent = key_parent[k],
+              jaro_col_name = jaro_col_name[k],
+              skip_probabilistic = key_baby[k] %in% deterministic_only
+            )
+            if (nrow(link_on_k$match) > 0) {
+              if (link_on_k$type == "deterministic") {
+                link_on_k$match[, jaro_col_name[k]] = 1
+              }
+              ## Append the Jaro score to the end of the candidates' data
+              candidates_parents_data_c = cbind(candidates_parents_data_c,
+                                                link_on_k$match[, jaro_col_name[k]])
+              colnames(candidates_parents_data_c)[ncol(candidates_parents_data_c)] = jaro_col_name[k]
+            } else {
+              ## Append the Jaro score to the end of the candidates' data
+              candidates_parents_data_c = cbind(candidates_parents_data_c,
+                                                NA)
+              colnames(candidates_parents_data_c)[ncol(candidates_parents_data_c)] = jaro_col_name[k]
             }
-            ## Append the Jaro score to the end of the candidates' data
-            candidates_parents_data_c = cbind(candidates_parents_data_c,
-                                              link_on_k$match[, jaro_col_name[k]])
-            colnames(candidates_parents_data_c)[ncol(candidates_parents_data_c)] = jaro_col_name[k]
-          } else {
-            ## Append the Jaro score to the end of the candidates' data
-            candidates_parents_data_c = cbind(candidates_parents_data_c,
-                                              NA)
-            colnames(candidates_parents_data_c)[ncol(candidates_parents_data_c)] = jaro_col_name[k]
           }
         }
+        ### Stack/save candidate
+        return_candidates_parents_data = rbind(return_candidates_parents_data,
+                                               candidates_parents_data_c)
       }
-      ### Stack/save candidate
-      return_candidates_parents_data = rbind(return_candidates_parents_data,
-                                             candidates_parents_data_c)
+      ## Calculate aggregate jaro scores per candidate
+      if (aggregate_scores == "mean") {
+        return_candidates_parents_data[, "jaro_aggregate"] = apply(
+          X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
+          MARGIN = 1,
+          na.rm = TRUE,
+          FUN = mean,
+          simplify = TRUE)
+      } else if (aggregate_scores == "median") {
+        return_candidates_parents_data[, "jaro_aggregate"] = apply(
+          X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
+          MARGIN = 1,
+          FUN = median,
+          na.rm = TRUE,
+          simplify = TRUE)
+      } else if (aggregate_scores == "sum") {
+        return_candidates_parents_data[, "jaro_aggregate"] = apply(
+          X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
+          MARGIN = 1,
+          FUN = sum,
+          na.rm = TRUE,
+          simplify = TRUE)
+      }
+
+      ## Order by jaro_aggregate
+      return_candidates_parents_data = return_candidates_parents_data[order(return_candidates_parents_data[, "jaro_aggregate"],
+                                                                            decreasing = TRUE), ]
+    } else {
+      return_candidates_parents_data = data.frame()
     }
 
-    ## Calculate aggregate jaro scores per candidate
-    if (aggregate_scores == "mean") {
-      return_candidates_parents_data[, "jaro_aggregate"] = apply(
-        X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
-        MARGIN = 1,
-        na.rm = TRUE,
-        FUN = mean,
-        simplify = TRUE)
-    } else if (aggregate_scores == "median") {
-      return_candidates_parents_data[, "jaro_aggregate"] = apply(
-        X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
-        MARGIN = 1,
-        FUN = median,
-        na.rm = TRUE,
-        simplify = TRUE)
-    } else if (aggregate_scores == "sum") {
-      return_candidates_parents_data[, "jaro_aggregate"] = apply(
-        X = as.matrix(return_candidates_parents_data[, jaro_col_name]),
-        MARGIN = 1,
-        FUN = sum,
-        na.rm = TRUE,
-        simplify = TRUE)
-    }
-
-    ## Order by jaro_aggregate
-    return_candidates_parents_data = return_candidates_parents_data[order(return_candidates_parents_data[, "jaro_aggregate"],
-                                                                          decreasing = TRUE), ]
+    ## Return the vector
+    return(
+      list(
+        best_candidate = return_candidates_parents_data[1, ],
+        all_candidates = return_candidates_parents_data
+      )
+    )
   } else {
-    return_candidates_parents_data = data.frame()
+    ## Return the vector
+    return(
+      list(
+        best_candidate = data.frame(),
+        all_candidates = data.frame()
+      )
+    )
   }
 
-  ## Return the vector
-  return(
-    list(
-      best_candidate = return_candidates_parents_data[1, ],
-      all_candidates = return_candidates_parents_data
-    )
-  )
+
 }
